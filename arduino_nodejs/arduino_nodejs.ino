27a581720b77447b6a35ac3f53b6d8c4da8c5059
alexruilli <alexruiz@jardesign.me>
2018-03-10 10:06:19 -0600

    Ejercicio1
    #1

--- arduino_nodejs/arduino_nodejs.ino
+++ arduino_nodejs/arduino_nodejs.ino
@@ -2,9 +2,19 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 Serial.println("Hola soy un Arduino");
+pinMode (13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+if (Serial.available()) {
+  char Letra = Serial.read();
+  Serial.print(Letra); 
+  if (Letra == 'H'){ 
+    digitalWrite(13, 1);
+    }
+  else if (Letra == 'L') {
+    digitalWrite(13, 0); 
+    }
+  }
 }
